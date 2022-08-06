4360abfa8fea76dd249881a1915f5c9ff3d495a8
ecunal <ecaliskanunal@gmail.com>
2022-08-06 14:34:17 +0100

    Instance variable, getter, setter and toString methods were added

--- src/galaxy01/PlanetX.java
+++ src/galaxy01/PlanetX.java
@@ -1,6 +1,22 @@
 package galaxy01;
 
 public class PlanetX {
+
+    private String troopName;
+
+    public PlanetX(String troopName) {
+        this.troopName = troopName;
+    }
+
+    public String getTroopName() {
+        return troopName;
+    }
+
+    public void setTroopName(String troopName) {
+        // add a condition in here
+        this.troopName = troopName;
+    }
+
     public PlanetX() {
         System.out.println("Contacted to her Majesty Queen El II");
     }
@@ -8,12 +24,14 @@ public class PlanetX {
         System.out.println("Invasion of Planet X");
     }
 
+    @Override
+    public String toString() {
+        return "PlanetX{}";
+    }
 
     public static void main(String[] args) {
         PlanetX planet = new PlanetX();
         planet.invade();
-
-
 
     }
 }
