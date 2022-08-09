8221e96aa369e1de7ed5926678139f461772a1e0
44784 <akiftunc76@gmail.com>
2022-08-09 13:11:43 +0100

    Starman's message

--- src/galaxy01/PlanetStardust.java
+++ src/galaxy01/PlanetStardust.java
@@ -4,6 +4,7 @@ public class PlanetStardust {
 
     public static void main(String[] args) {
         System.out.println(" Hello world! Be ready! ");
+        System.out.println("Starman is walking in the sky!");
 
     }
 }
